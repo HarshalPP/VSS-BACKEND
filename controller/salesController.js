8737b63279efc16fdf9fc1d2c23d32d47db67e4c
Harshal <harshalpawar>
2024-12-31 00:12:57 +0530

    add

--- controller/salesController.js
+++ controller/salesController.js
@@ -175,6 +175,7 @@ exports.create = async (req, res) => {
         const browser = await puppeteer.launch({
             headless: 'new', 
             args: ['--no-sandbox'],// Set to true if you want to run iya n headless mode
+            executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe', // Update this path
             
         });
 
